77b23718dba565e27545578af627c2ab0835c4a1
FlameBuster <akulrajeevan8@gmail.com>
2024-10-14 18:47:20 +0530

    New Project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,3 @@
-
-
 const Projects = () => {
   return (
     <section id="projects">
@@ -10,16 +8,43 @@ const Projects = () => {
         <div className="project-card">
           <h2>CAM Project</h2>
           <p>
-          A Web Application ,Corps Archival Museum  to facilitate the digitization and management of various artifacts.
-
+            A Web Application ,Corps Archival Museum to facilitate the
+            digitization and management of various artifacts.
           </p>
-          <p><strong>Tech Stack:</strong> Next.js, Node.js, MongoDB</p>
-          <a href="https://cam-build-flamebusters-projects.vercel.app/" target="_blank" rel="noopener noreferrer" className="project-button">
+          <p>
+            <strong>Tech Stack:</strong> Next.js, Node.js, MongoDB
+          </p>
+          <a
+            href="https://cam-build-flamebusters-projects.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-button"
+          >
             Visit Project
           </a>
         </div>
 
-        
+        <div className="project-card">
+          <h2>Youtube-Summarizer</h2>
+          <p>
+            The YouTube Summarizer App simplifies long videos by generating
+            concise, easy-to-read summaries. Powered by advanced natural
+            language processing, it quickly analyzes video content and produces
+            clear summaries
+          </p>
+          <p>
+            <strong>Tech Stack:</strong> React, Tailwind CSS, Node.js,
+            Express.js Python ( Hugging Face).
+          </p>
+          <a
+            href="https://yt-summarizer-frontend.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-button"
+          >
+            Visit Project
+          </a>
+        </div>
       </div>
     </section>
   );
